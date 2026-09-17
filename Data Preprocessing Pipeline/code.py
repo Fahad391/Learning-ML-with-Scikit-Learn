c252172fed24285813a14dc5369cf569e9349ba4
Md Farhan Rahman (Fahad) <mdfahadr12@gmail.com>
2026-09-18 03:32:29 +0600

    Add files via upload

---
+++ Data Preprocessing Pipeline/code.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+# Creating the messy data
+data = pd.DataFrame({
+    "Area": ["Uttara", "Gulshan", np.nan, "Badda", np.nan, "Gulshan", "Banani"],
+    "Item purchased": [np.nan, 3, 5, np.nan, 1, 5, 2],
+    "Total Cost (BDT)": [1800, np.nan, 7000, 600, 9500, 11500, 2200]
+})
+
+number_imputer = SimpleImputer(strategy="mean") 
+category_imputer = SimpleImputer(strategy="most_frequent") # Area as category
+
+# Applying it on the data using fit_transform
+data[["Area"]] = category_imputer.fit_transform(data[["Area"]])
+data[["Item purchased"]] = number_imputer.fit_transform(data[["Item purchased"]])
+data[["Total Cost (BDT)"]] = number_imputer.fit_transform(data[["Total Cost (BDT)"]])
+
+# Encode & Scale
+encoder = OneHotEncoder(sparse_output=False)
+
+encode_category = encoder.fit_transform(data[["Area"]])
+new_column = encoder.get_feature_names_out(["Area"]) # makes the new column
+
+# Reframe Data
+new_df_1 = pd.DataFrame(encode_category, columns=new_column)
+
+# Scale
+scaler = StandardScaler()
+
+num_scaler = scaler.fit_transform(data[["Item purchased", "Total Cost (BDT)"]])
+
+# Reframe again
+new_df_2 = pd.DataFrame(num_scaler, columns=["Item purchased",  "Total Cost (BDT)"])
+
+# Assemble the data usinfg concate
+X = pd.concat([new_df_1, new_df_2], axis=1)
+
+print(X)
---
+++ Data Preprocessing Pipeline/idea.md
@@ -0,0 +1,12 @@
+1) Create a messy data of customers containing:
+    1) Area
+    2) Item purchased
+    3) Total Spent in BDT
+
+2) Impute to fill the missing part of the dataset
+
+3) Feature Transformation:
+    1) Encode Categories
+    2) Scale Numbers
+
+4) Assemble the Data
